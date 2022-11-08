5708c631902825d6061f32e1a61964abd702e1ef
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-11-08 01:13:27 -0300

    feat: creation of participant name input

--- App.tsx
+++ App.tsx
@@ -1,28 +1,5 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Olá mundo</Text>
-      <Text style={styles.secondaryText}>Meu primeiro projeto em React Native</Text>
-      <StatusBar barStyle="light-content" />
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#131016',
-  },
-  text: {
-    fontSize: 32,
-    color: '#cccccc',
-  },
-  secondaryText: {
-    fontSize: 16,
-    color: '#ffffff',
-  },
-});
